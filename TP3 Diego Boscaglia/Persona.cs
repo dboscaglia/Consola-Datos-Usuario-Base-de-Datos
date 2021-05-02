e8eb8b081b27beddc70db19399dec3867fd0f886
Diego Boscaglia <drboscaglia@gmail.com>
2021-05-02 14:32:59 -0300

    Agregar archivos de proyecto.

---
+++ TP3 Diego Boscaglia/Persona.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_Diego_Boscaglia
+{
+    class Persona
+    {
+        private string nombre;
+        private string email;
+        private float sueldo;
+        private string sexo;
+        private float peso;
+        private float altura;
+
+        public Persona(string nombre, string email, float sueldo, string sexo, float peso, float altura)
+        {
+            this.nombre = nombre;
+            this.email = email;
+            this.sueldo = sueldo;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+            CrearEmail();
+
+        }
+         public string GetNombre()
+
+        {
+            return nombre;
+        }
+
+        public string GetEmail()
+
+        {
+            return email;
+
+        }
+
+        public float GetSueldo()
+
+        {
+            return sueldo;
+
+        }
+
+        public string GetSexo()
+
+        {
+            return sexo;
+
+        }
+
+
+        public float GetPeso()
+
+        {
+            return peso;
+
+        }
+
+        public float GetAltura()
+
+        {
+            return altura;
+
+        }
+
+        private void CrearEmail()
+
+        {
+            email = nombre + "@superempresa.com";
+        }
+
+    }
+}
+
+
+
+
+
---
+++ TP3 Diego Boscaglia/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_Diego_Boscaglia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre; string email; float sueldo; string sexo; float peso; float altura;
+
+            Console.WriteLine("Ingrese Nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese Email:");
+            email = Console.ReadLine();
+            Console.WriteLine("Ingrese Sexo:");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Ingrese Sueldo:");
+            sueldo = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Peso:");
+            peso = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Altura:");
+            altura = float.Parse(Console.ReadLine());
+
+
+            Persona miPersona = new Persona(nombre, email, sueldo, sexo, peso, altura) ;
+            
+
+            Console.WriteLine(nombre);
+            Console.WriteLine(email);
+            Console.WriteLine(sueldo);
+            Console.WriteLine(sexo);
+            Console.WriteLine(peso);
+            Console.WriteLine(altura);
+            Console.WriteLine("Los datos ingresados son");
+            Console.WriteLine(miPersona.GetNombre());
+            Console.WriteLine(miPersona.GetEmail());
+            Console.WriteLine(miPersona.GetSueldo());
+            Console.WriteLine(miPersona.GetSexo());
+            Console.WriteLine(miPersona.GetPeso());
+            Console.WriteLine(miPersona.GetAltura());
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+
+    }
+}
